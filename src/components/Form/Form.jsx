00fcef66478cc9db0de90b6d5915bb8d3aaa9010
zacharybaca <zachary.baca@msn.com>
2024-07-24 09:51:19 -0500

    designed form inputs and styled

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,7 +3,15 @@ import "./form.css";
 export default function Form() {
     return (
         <div id="form-container">
-            <h1>Form</h1>
+            <form id="form-inputs" name="ugly-things-form">
+                <input type="text" name="title" id="title-input" placeholder="Title" required={true} />
+                <input type="text" name="description" id="description-input" placeholder="Description" required={true} />
+                <input type="text" name="imageUrl" id="image-url-input" placeholder="Image URL" required={true} />
+               
+            </form>
+            <div id="button-container">
+                    <button type="submit">Submit</button>
+                </div>
         </div>
     )
 }
