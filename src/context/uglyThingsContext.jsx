dd6df0e8a2d7d16e9cb85f72565dbdc46b034bc4
zacharybaca <zachary.baca@msn.com>
2024-07-24 08:36:21 -0500

    set up api to get things

--- src/context/uglyThingsContext.jsx
+++ src/context/uglyThingsContext.jsx
@@ -1,13 +1,27 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const UglyThingsContext = React.createContext();
 
 function UglyThingsContextProvider(props) {
+    const [uglyThings, setUglyThings] = useState();
+
+    useEffect(() => {
+        const getUglyThings = async () => {
+            const data = await fetch("https://api.vschool.io/zacharybaca/thing");
+            const json = await data.json();
+            setUglyThings(json);
+            console.log(uglyThings)
+        }
+        getUglyThings();
+    }, [uglyThings])
+
     return (
-        <UglyThingsContext.Provider>
+        <UglyThingsContext.Provider value={{
+            uglyThings: uglyThings
+        }}>
             {props.children}
         </UglyThingsContext.Provider>
     )
